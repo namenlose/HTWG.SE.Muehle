33a55738a6d1cef6d10cc9ee66d34b83f1e689e3
Sofie <sbirkle@gmx.net>
2023-10-20 08:14:49 +0200

    senkrechte striche

--- src/main/scala/MainField.scala
+++ src/main/scala/MainField.scala
@@ -2,10 +2,15 @@
   println("Muehle")
   println(mesh)
 
+//val barWidth = 8
+
 val eol = sys.props("line.separator")
+//def bar(barWidth: Int) = "o" + "-" * barWidth + "o" + "-" * barWidth + "o" + eol
 def bar(cellWidth: Int = 6) = "o" + "-" * cellWidth + "o" + "-" * cellWidth + "o" + eol
+def line(cellWidth: Int = 6) = "|" + " " * cellWidth + "|" + " " * cellWidth + "|" + eol 
 def cells(cellWidth: Int = 4) = "| o" + "-" * cellWidth + "o" + "-" * cellWidth + "o" + " |" + eol /*"|-o---o---o-|"*/
+def line2(cellWidth: Int = 4) = "| |" + " " * cellWidth + "|" + " " * cellWidth + "| |" + eol
 //def cells2(cellWidth:Int = 4) = "| |" + "-" * cellWidth + "o" + "-" * cellWidth + "| |" + eol /*"| |---o---| |"*/
 def cells3(cellWidth:Int = 2) = "| | " + "o" + "-" * cellWidth + "o" + "-" * cellWidth + "o" + " | |" + eol   /*"| | o-o-o | |"*/
 def cells4(cellNumber:Int = 2, cellWidth:Int = 5) = ("o-o-o" + " " * cellWidth)* cellNumber + eol
-val mesh = (bar() + cells() + cells3() + cells4()) + (cells3() + cells() + bar())
+val mesh = (bar(8) + line(8) + cells(6) + line2(6) + cells3(4) + cells4(2, 9)) + (cells3(4) + line2(6) + cells(6) + line(8) + bar(8))
