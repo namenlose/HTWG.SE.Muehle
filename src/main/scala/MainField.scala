b48d756f0b4a81d5e7d2e03e3a1a341e591fc170
Sofie <sbirkle@gmx.net>
2023-10-20 08:51:32 +0200

    variable größe

--- src/main/scala/MainField.scala
+++ src/main/scala/MainField.scala
@@ -2,6 +2,7 @@
   println("Hallo, das ist Muehle")
   println(mesh)
 
+val groeße = 6
 
 val eol = sys.props("line.separator")
 def bar(cellWidth: Int = 6) = "o" + "-" * cellWidth + "o" + "-" * cellWidth + "o" + eol
@@ -11,4 +12,4 @@ def line2(cellWidth: Int = 4) = "| |" + " " * cellWidth + "|" + " " * cellWidth 
 //def cells2(cellWidth:Int = 4) = "| |" + "-" * cellWidth + "o" + "-" * cellWidth + "| |" + eol /*"| |---o---| |"*/
 def cells3(cellWidth:Int = 2) = "| | " + "o" + "-" * cellWidth + "o" + "-" * cellWidth + "o" + " | |" + eol   /*"| | o-o-o | |"*/
 def cells4(cellNumber:Int = 2, cellWidth:Int = 5) = ("o-o-o" + " " * cellWidth)* cellNumber + eol
-val mesh = (bar(8) + line(8) + cells(6) + line2(6) + cells3(4) + cells4(2, 9)) + (cells3(4) + line2(6) + cells(6) + line(8) + bar(8))
+val mesh = (bar(groeße) + line(groeße) + cells(groeße - 2) + line2(groeße - 2) + cells3(groeße - 4) + cells4(2, groeße + 1)) + (cells3(groeße -4) + line2(groeße - 2) + cells(groeße - 2) + line(groeße) + bar(groeße))
