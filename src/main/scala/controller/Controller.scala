570bf1a8f5e6dc988316567d94c3b536b17061f4
Sofie <sbirkle@gmx.net>
2023-11-24 09:46:12 +0100

    Handler Pettern

--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,31 +1,34 @@
 package HTWG.SE.Muehle.controller
-import HTWG.SE.Muehle.model.{FieldArray, Field}
+import HTWG.SE.Muehle.model.{FieldArray, Field, Handler1}
 import HTWG.SE.Muehle.util.Observable
+
 
 class Controller() extends Observable{
 
     val array: FieldArray = new FieldArray()
     val field1: Field = new Field(6, array.fieldArray)
+    val handler1: Handler1 = new Handler1(array.fieldArray)
     var fieldString = ""
     
     def controllerPlaceFirstStone(ind1: Int, ind2: Int, player: Char): String = {
         
         fieldString = array.placeStone(ind1, ind2, player)
-        //val field2: Field = new Field(6, array.fieldArray)
         notifyObservers
         fieldString
     }
 
     def controllerPlaceStones(ind1: Int, ind2: Int, player1: Char, player2: Char, i: Int, mesh: String): String = {
-        //var mesh2 = mesh
+
         if(i % 2 == 0){
             fieldString = array.placeStone(ind1, ind2, player1)
-            //val i: Field = new Field(6, array.fieldArray)
-           // mesh2 = i.mesh
+            if(handler1.checkRequirement(array.fieldArray) == true){
+                println("MÜHLE!!")
+            }
         }else{
             fieldString = array.placeStone(ind1, ind2, player2)
-            //val i: Field = new Field(6, array.fieldArray)
-            //mesh2 = i.mesh
+             if(handler1.checkRequirement(array.fieldArray) == true){
+                println("MÜHLE!!")
+            }
         }
         notifyObservers
         fieldString
