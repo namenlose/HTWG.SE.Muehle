184df4c09878f016563ab0c819205104f3946f82
Sofie <sbirkle@gmx.net>
2023-12-01 12:21:48 +0100

    Option?

--- src/main/scala/util/UndoManager.scala
+++ src/main/scala/util/UndoManager.scala
@@ -4,16 +4,24 @@ import HTWG.SE.Muehle.controller.Controller
 class UndoManager(controller: Controller) {
     var undoStack: List[Command] = List()
     var redoStack: List[Int] = List()
+    var empty: Option[List[Command]] = None
 
     def doStep(command: Command) ={
         undoStack = command +: undoStack
+        empty = Some(undoStack)
         command.doStep
         undoStack
     }
 
     def undoStep: String = {
-        val command = undoStack.head
-        undoStack = undoStack.drop(1)
-        command.undoStep
+        var string = ""
+        if(empty == Some(undoStack)){
+            val command = undoStack.head
+            undoStack = undoStack.drop(1)
+            string = command.undoStep
+        }else{
+            println("lehre liste")
+        }
+        string
     }
 }
